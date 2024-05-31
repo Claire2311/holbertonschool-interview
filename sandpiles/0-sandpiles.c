336e70f752bf40d224c65551bbb77b1a21e31865
Claire2311 <claire.girard@ieseg.fr>
2024-05-31 13:14:15 +0200

    Task 0

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -7,79 +7,71 @@
 #include <stdio.h>
 #include "sandpiles.h"
 
-void print_grid(int grid[3][3])
-{
-    int i, j;
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            if (j)
-            {
-                printf(" ");
-            }
-            printf("%d", grid[i][j]);
-        }
-        printf("\n");
-    }
-}
-
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-    int grid[3][3];
-    int unstable = 1;
     int i, j;
-    int temp[3][3];
 
+    // Step 1: Add grid2 to grid1
     for (i = 0; i < 3; i++)
     {
         for (j = 0; j < 3; j++)
         {
-            grid[i][j] = grid1[i][j] + grid2[i][j];
+            grid1[i][j] += grid2[i][j];
         }
     }
 
-    while (unstable)
+    // Step 2: Stabilize grid1
+    while (1)
     {
-        unstable = 0;
+        int unstable = 0;
+        int temp_grid[3][3] = {0};
 
+        // Check for instability and prepare the toppling
         for (i = 0; i < 3; i++)
         {
             for (j = 0; j < 3; j++)
             {
-                if (grid[i][j] > 3)
+                if (grid1[i][j] > 3)
                 {
-                    grid[i][j] -= 4;
-                    if (i > 0)
-                    {
-                        temp[i - 1][j] += 1;
-                    }
-                    if (i < 2)
-                    {
-                        temp[i + 1][j] += 1;
-                    }
-                    if (j > 0)
-                    {
-                        temp[i][j - 1] += 1;
-                    }
-                    if (j < 2)
-                    {
-                        temp[i][j + 1] += 1;
-                    }
                     unstable = 1;
+                    temp_grid[i][j] -= 4;
+                    if (i > 0)
+                        temp_grid[i - 1][j]++;
+                    if (i < 2)
+                        temp_grid[i + 1][j]++;
+                    if (j > 0)
+                        temp_grid[i][j - 1]++;
+                    if (j < 2)
+                        temp_grid[i][j + 1]++;
                 }
             }
         }
 
+        // If no cells are unstable, break out of the loop
+        if (!unstable)
+            break;
+
+        // Print the unstable grid
+        printf("=\n");
+
         for (i = 0; i < 3; i++)
         {
             for (j = 0; j < 3; j++)
             {
-                grid[i][j] += temp[i][j];
+                if (j)
+                    printf(" ");
+                printf("%d", grid1[i][j]);
             }
+            printf("\n");
         }
 
-        print_grid(grid);
-        printf("\n");
+        // Apply the toppling
+        for (i = 0; i < 3; i++)
+        {
+            for (j = 0; j < 3; j++)
+            {
+                grid1[i][j] += temp_grid[i][j];
+            }
+        }
     }
 }
--- sandpiles/sandpiles.h
+++ sandpiles/sandpiles.h
@@ -2,5 +2,7 @@
 #define SANDPILES_H
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+void print_grid(int grid[3][3]);
+void stabilize_sandpile(int grid[3][3]);
 
 #endif
