5073f371fc08bff71dd56a67b142c00968f46d71
Claire2311 <claire.girard@ieseg.fr>
2024-08-01 11:34:58 +0200

    TAsk 0

--- island_perimeter/0-island_perimeter.py
+++ island_perimeter/0-island_perimeter.py
@@ -8,12 +8,14 @@ def island_perimeter(grid):
     """Function to solve the Island Perimeter algorithm"""
     a = 0
     number_of_lines = len(grid) - 1
+    number_of_columns = len(grid[1]) - 1
     # print("number_of_lines", number_of_lines)
+    # print("number_of_columns", number_of_columns)
 
     coordinates = []
 
     for i in range(len(grid)):
-        for j in range(len(grid)):
+        for j in range(len(grid[0])):
             if grid[i][j] == 1:
                 coordinates.append([i, j])
     # print("coordinates", coordinates)
@@ -23,16 +25,26 @@ def island_perimeter(grid):
         # print(grid[abs[0]][abs[1] - 1])  # gauche
         # print(grid[abs[0] - 1][abs[1]])  # haut
         # print(grid[abs[0] + 1][abs[1]])  # bas
-        if grid[abs[0]][abs[1] + 1] == 0:
+        if abs[0] == 0:
+            # print("abs[0] = 0", abs)
             a += 1
-        if grid[abs[0]][abs[1] - 1] == 0:
+        if abs[1] == 0:
+            # print("abs[1] = 0", abs)
             a += 1
-        if grid[abs[0] - 1][abs[1]] == 0:
+        if abs[1] + 1 > number_of_columns or grid[abs[0]][abs[1] + 1] == 0:  # droite
+            # print("abs droite", abs)
             a += 1
-        if abs[0] + 1 > number_of_lines or grid[abs[0] + 1][abs[1]] == 0:
+        if grid[abs[0]][abs[1] - 1] == 0:  # gauche
+            # print("abs gauche", abs)
+            a += 1
+        if grid[abs[0] - 1][abs[1]] == 0:  # haut
+            # print("abs haut", abs)
+            a += 1
+        if abs[0] + 1 > number_of_lines or grid[abs[0] + 1][abs[1]] == 0:  # bas
+            # print("abs bas", abs)
             a += 1
 
-    # print(a)
+    # print("a", a)
     return a
 
 
@@ -58,5 +70,17 @@ def island_perimeter(grid):
 #     [0, 1, 1, 1, 0, 0],
 #     [0, 1, 1, 1, 0, 0]
 #     ]
+
+# grid = [[1, 1, 1, 1, 1], [1, 1, 1, 1, 1], [1, 1, 1, 1, 1]]
+
+# grid = [
+#     [0, 0, 0, 0, 0, 0],
+#     [0, 1, 1, 0, 0, 0],
+#     [1, 1, 1, 0, 0, 0],
+#     [0, 1, 1, 1, 0, 0],
+#     [0, 0, 0, 1, 1, 1]
+#         ]
+
+# grid = [[0, 1, 0, 0, 0, 1],[1, 1, 0, 0, 0, 1],[1, 1, 0, 1, 1, 1],[0, 1, 1, 1, 0, 0],[0, 0, 1, 1, 0, 0]]
 
 # island_perimeter(grid)
